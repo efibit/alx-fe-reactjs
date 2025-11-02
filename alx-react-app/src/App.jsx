2eee9991ba6aa072eb0c1f17aac91d73fadaf4b4
efibit <efratahabtu72@gmail.com>
2025-11-02 20:02:19 +0000

    Add UserProfile component with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,8 @@ import './App.css'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
 
 
 function App() {
@@ -18,6 +20,12 @@ function App() {
             <Header />
       <MainContent />
       <Footer />
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+
 
       <div>
         <a href="https://vite.dev" target="_blank">
